b51f26b83010f16ac8f32ac5134a91812e588959
Michael Allan <michaelallan@optushome.com.au>
2014-03-21 11:10:25 +1100

    Add browserify grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,22 @@ module.exports = function (grunt) {
             client: [
                 'Gruntfile.js'
             ],
+        },
+
+        browserify: {
+            options: {
+                transform: [ require('grunt-react').browserify ]
+            },
+            client: {
+                src: ['react_components/**/*.jsx'],
+                dest: 'scripts/app.built.js'
+            }
         }
     });
 
     // load
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-browserify');
 
     // register
     grunt.registerTask('default', ['jshint']);
